660a08c478bf322c5b7cef1d3cb78761cb4837e6
DYZ <897917728@qq.com>
2023-02-02 10:41:28 +0800

    配置了restful

--- boot/src/main/java/com/boot/config/ShiroConfig.java
+++ boot/src/main/java/com/boot/config/ShiroConfig.java
@@ -1,53 +1,53 @@
-//package com.boot.config;
-//
-//
-//import com.boot.common.MyRealm;
-//import org.apache.shiro.realm.Realm;
-//import org.apache.shiro.spring.web.ShiroFilterFactoryBean;
-//import org.apache.shiro.web.mgt.DefaultWebSecurityManager;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//
-//import java.util.HashMap;
-//import java.util.Map;
-//
-///**
-// * @Project: word
-// * @Author: DengYinzhe
-// * @Date: 2023/2/1 14:31
-// * @FileName: ShiroConfig
-// * @Description: shiro配置类
-// */
-//@Configuration
-//public class ShiroConfig {
-//    ///ShiroFilter过滤所有请求
-//    @Bean
-//    public ShiroFilterFactoryBean getShiroFilterFactoryBean(DefaultWebSecurityManager securityManager) {
-//        ShiroFilterFactoryBean shiroFilterFactoryBean = new ShiroFilterFactoryBean();
-//        //给ShiroFilter配置安全管理器
-//        shiroFilterFactoryBean.setSecurityManager(securityManager);
-//        //配置系统受限资源
-//        //配置系统公共资源
-//        Map<String, String> map = new HashMap<String, String>();
-//        map.put("/index","authc");//表示这个资源需要认证和授权
-//        // 设置认证界面路径
-//        shiroFilterFactoryBean.setLoginUrl("/login.jsp");
-//        shiroFilterFactoryBean.setFilterChainDefinitionMap(map);
-//
-//        return shiroFilterFactoryBean;
-//    }
-//    //创建安全管理器
-//    @Bean
-//    public DefaultWebSecurityManager getDefaultWebSecurityManager(Realm realm) {
-//        DefaultWebSecurityManager securityManager = new DefaultWebSecurityManager();
-//        securityManager.setRealm(realm);
-//        return securityManager;
-//    }
-//    //创建自定义Realm
-//    @Bean
-//    public Realm getRealm() {
-//        MyRealm realm = new MyRealm();
-//        return realm;
-//    }
-//}
-//
+package com.boot.config;
+
+
+import com.boot.common.MyRealm;
+import org.apache.shiro.realm.Realm;
+import org.apache.shiro.spring.web.ShiroFilterFactoryBean;
+import org.apache.shiro.web.mgt.DefaultWebSecurityManager;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @Project: word
+ * @Author: DengYinzhe
+ * @Date: 2023/2/1 14:31
+ * @FileName: ShiroConfig
+ * @Description: shiro配置类
+ */
+@Configuration
+public class ShiroConfig {
+    ///ShiroFilter过滤所有请求
+    @Bean
+    public ShiroFilterFactoryBean getShiroFilterFactoryBean(DefaultWebSecurityManager securityManager) {
+        ShiroFilterFactoryBean shiroFilterFactoryBean = new ShiroFilterFactoryBean();
+        //给ShiroFilter配置安全管理器
+        shiroFilterFactoryBean.setSecurityManager(securityManager);
+        //配置系统受限资源
+        //配置系统公共资源
+        Map<String, String> map = new HashMap<String, String>();
+        map.put("/index","authc");//表示这个资源需要认证和授权
+        // 设置认证界面路径
+        shiroFilterFactoryBean.setLoginUrl("/login.jsp");
+        shiroFilterFactoryBean.setFilterChainDefinitionMap(map);
+
+        return shiroFilterFactoryBean;
+    }
+    //创建安全管理器
+    @Bean
+    public DefaultWebSecurityManager getDefaultWebSecurityManager(Realm realm) {
+        DefaultWebSecurityManager securityManager = new DefaultWebSecurityManager();
+        securityManager.setRealm(realm);
+        return securityManager;
+    }
+    //创建自定义Realm
+    @Bean
+    public Realm getRealm() {
+        MyRealm realm = new MyRealm();
+        return realm;
+    }
+}
+
