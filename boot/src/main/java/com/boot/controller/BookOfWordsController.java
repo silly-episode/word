2462eb889ad43c29ef76e60ed5c61a0f9b9446e3
DYZ <897917728@qq.com>
2023-02-25 15:33:19 +0800

    用户模块

--- boot/src/main/java/com/boot/controller/BookOfWordsController.java
+++ boot/src/main/java/com/boot/controller/BookOfWordsController.java
@@ -1,11 +1,15 @@
 package com.boot.controller;
 
 
+import com.boot.common.result.Result;
+import com.boot.entity.BookOfWords;
 import com.boot.service.BookOfWordsService;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * (BookOfWords)表控制层
@@ -21,6 +25,52 @@ public class BookOfWordsController {
      */
     @Resource
     private BookOfWordsService bookOfWordsService;
+
+
+    /**
+     * @param wordId:
+     * @Return: Result
+     * @Author: DengYinzhe
+     * @Description: TODO 取消收藏
+     * @Date: 2023/2/25 15:13
+     */
+    @DeleteMapping("word/{wordId}")
+    public Result words(@PathVariable Long wordId) {
+        if (bookOfWordsService.removeById(wordId)) {
+            return Result.success("删除成功");
+        }else {
+            return Result.error("删除失败");
+        }
+    }
+
+    /**
+     * @param bookId:
+     * @Return: Result
+     * @Author: DengYinzhe
+     * @Description: TODO 获取练习的单词
+     * @Date: 2023/2/25 15:20
+     */
+    @GetMapping("word/{bookId}")
+    public Result wordss(@PathVariable Long bookId) {
+        Map<String, Object> map = new HashMap<>();
+        map.put("book_id", bookId);
+        List<BookOfWords> list = bookOfWordsService.listByMap(map);
+        return Result.success(list);
+    }
+
+    /**
+     * @param bookId:
+     * @Return: Result
+     * @Author: DengYinzhe
+     * @Description: TODO 单词本导出为pdf
+     * @Date: 2023/2/25 15:30
+     */
+    @GetMapping("book/{bookId}")
+    public Result book (@PathVariable Long bookId){
+        
+        return Result.success();
+    }
+
 
 }
 
